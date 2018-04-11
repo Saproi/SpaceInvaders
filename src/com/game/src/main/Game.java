51a45a214de79177e38b02762767bc9438ba282f
Saproi <stefjims@gmail.com>
2018-04-11 15:08:02 -0600

    Balas

--- src/com/game/src/main/Game.java
+++ src/com/game/src/main/Game.java
@@ -25,6 +25,7 @@ public class Game extends Canvas implements Runnable {
 	private BufferedImage spriteSheet = null;
 	
 	private Player p;
+	private Controller c;
 	
 	
 	public void init() {
@@ -39,6 +40,7 @@ public class Game extends Canvas implements Runnable {
 		
 		addKeyListener(new KeyInput(this));
 		p = new Player(200, 200 ,this);
+		c = new Controller(this);
 	}
 	
 	
@@ -98,6 +100,7 @@ public class Game extends Canvas implements Runnable {
 	
 	private void tick() {
 		p.tick();
+		c.tick();
 	}
 	
 	private void render() {
@@ -114,7 +117,7 @@ public class Game extends Canvas implements Runnable {
 		/////////////////////////////////
 		g.drawImage(image, 0, 0, getWidth(),getHeight(),this);
 		p.render(g);
-		
+		c.render(g);
 		
 		
 		
@@ -136,6 +139,8 @@ public class Game extends Canvas implements Runnable {
 			p.setVelY(5);
 		}else if(key == KeyEvent.VK_UP) {
 			p.setVelY(-5);
+		}else if(key == KeyEvent.VK_SPACE) {
+			c.addBullet(new Bullet(p.getX(),p.getY(),this));
 		}
 	}
 	
